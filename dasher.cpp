c6d3bb84d8cc7afb880a556e15d3835c695c2e1a
Miguel Alonso <malon025@fiu.edu>
2021-08-08 14:49:08 -0400

    Updated character logic and audio intro

--- dasher.cpp
+++ dasher.cpp
@@ -3,58 +3,93 @@
 
 int main()
 {
-    int screenWidth{800};
-    int screenHeight{400};
- InitWindow(screenWidth, screenHeight, "Miguel's little window");
- Texture2D player = LoadTexture("textures/dude.png");
-    Rectangle rect;
-    rect.height = player.height;
-    rect.width = player.width / 6;
-    rect.x = 0;
-    rect.y = 0;
-    Vector2 rectPosition;
-    rectPosition.x = screenWidth/2 - rect.width / 2;
-    rectPosition.y = screenHeight - rect.height;
+    constexpr int screenWidth{800};
+    constexpr int screenHeight{400};
+    constexpr int gravity {4};
+    int velocity {10};
+    int speed {};
+    int jumpHeight {-200};//negative cuz jump goes in the negative Y axis direction
+    bool isInAir {};//false by default
+    Music music;
 
-bool isInAir {};
-   
+
+    InitWindow(screenWidth, screenHeight, "Miguel's little window");
+    InitAudioDevice();              // Initialize audio device
+    Texture2D playerTexture = LoadTexture("textures/dude.png");
+    music = LoadMusicStream("intro.wav");
+    PlayMusicStream(music);     
+    Rectangle rect//variable declaration
+    {
+        0,//x coordinate
+        0,//y coordinate
+        playerTexture.width / 6,//width
+        playerTexture.height//height
+    };
+
+    Vector2 rectPosition//position of character
+    {
+        screenWidth/2 - rect.width / 2,//x coordinate
+        screenHeight - rect.height//y coordinate
+    };
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         //game logic begins here
-
-        //if ()
-        if (IsKeyPressed(KEY_SPACE))// !isInAir)
+  UpdateMusicStream(music);
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
         {
-               // isInAir = true;
-            rectPosition.y += -20; 
+            isInAir = true;
+            rectPosition.y += jumpHeight;
         }
 
-           if (IsKeyDown(KEY_A))// !isInAir)
+        if (IsKeyDown(KEY_A))//move character left
         {
-               if(rect.x == player.width)
-               rect.x = 0;
-                rect.x += player.width / 6;
-            rectPosition.x -= 4; 
+          
+            if (rect.x == playerTexture.width) //if rectangle in last sprite
+                 rect.x = 0;
+            else 
+                if (!isInAir)//don't update sprite while player is jumping
+                    rect.x += playerTexture.width / 6;
+
+             rectPosition.x -= velocity;//move left
+             speed = 10;
         }
 
-          if (IsKeyDown(KEY_D))// !isInAir)
+        if (IsKeyDown(KEY_D))
         {
-               // isInAir = true;
-               if(rect.x == player.width)
-               rect.x = 0;
-                  rect.x += player.width / 6;
-            rectPosition.x += 4; 
-        }
-       DrawTextureRec(player, rect, rectPosition, WHITE);
-        ClearBackground(WHITE);
+            
+             if (rect.x == playerTexture.width) //if rectangle in last sprite
+                 rect.x = 0;
+            else 
+                if (!isInAir)//don't update sprite while player is jumping
+                    rect.x += playerTexture.width / 6;
 
-        EndDrawing();
+             rectPosition.x += velocity;//move right
+             speed = 10;
+        }
+        //ground check
+        if (rectPosition.y < screenHeight - rect.height)
+        {
+            rectPosition.y += gravity;
+        }
+        else{isInAir = false;};
+
+       DrawTextureRec(playerTexture, rect, rectPosition, WHITE);
+
+       if (speed == 0)
+            rect.x = 0;
+      else
+            speed--;
+       ClearBackground(WHITE);
+
+       EndDrawing();
 
     } //end game loop
-UnloadTexture(player);
+     UnloadMusicStream(music);   // Descargo la musica de RAM
+    UnloadTexture(playerTexture);
     CloseWindow();
 
 } //end main
